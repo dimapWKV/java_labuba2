dbece3dcb2936b0c606c8240178b37efbb13fe37
wikivik-git <dimap3011@gmail.com>
2026-09-15 20:43:49 +0700

    first commit

---
+++ src/ru/university/lab2/Main.java
@@ -0,0 +1,18 @@
+package ru.university.lab2;
+
+public class Main {
+    public void main() {
+        Byte bmin = Byte.MIN_VALUE;
+        Byte bmax = Byte.MAX_VALUE;
+        Short smin = Short.MAX_VALUE;
+        Short smax = Byte.MAX_VALUE;
+        Long lmin = Long.MIN_VALUE;
+        Long lmax = Long.MAX_VALUE;
+        Integer int_min = Integer.MIN_VALUE;
+        Integer int_max = Integer.MAX_VALUE;
+
+        System.out.println("");
+    }
+}
+
+//x & (x - 1)
